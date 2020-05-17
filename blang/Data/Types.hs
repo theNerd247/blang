638357a43e704d5a7b7492357dd4db7b796f1e59
theNerd247 <noah.harvey247 gm>
2020-05-16 19:08:05 -0700

    refactors types modules

---
+++ blang/Data/Types.hs
@@ -0,0 +1,46 @@
+module Data.Types where
+
+import Data.Decimal
+import Data.Functor.Foldable.TH
+import Data.Time.Calendar
+import Relude
+
+data Term
+  = Lit Lit
+  | Var Var
+  | Lam Var Term
+  | App Term Term
+  | Container (Container Term)
+ 
+data Lit 
+  = Num Double
+  | Bool Bool
+  | Text LText
+  | Account Account
+  | Amount Amount   
+  | Trans Transaction
+  | Date Date
+ 
+data Container a 
+  = List [a]
+  deriving (Functor, Foldable, Traversable)
+ 
+data Transaction = Transaction
+  { date        :: Date
+  , fromAccount :: Account
+  , toAccount   :: Account
+  , description :: Text
+  , amount      :: Amount
+  } deriving (Show, Eq, Ord)
+
+type Date = Day
+
+type Var = LText
+ 
+type Amount = Decimal
+ 
+type Account = Text 
+ 
+makeBaseFunctor ''Term
+
+
