a65f401b7efc04043336a35c12e629556a0aee7c
theNerd247 <noah.harvey247 gm>
2020-05-16 19:02:13 -0700

    finished basic types

--- blang/Main.hs
+++ blang/Main.hs
@@ -1,142 +1,52 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE DeriveFoldable #-}
-{-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE GADTs #-}
-
-
 module Main where
 
 import Data.Decimal
+import Data.Functor.Foldable.TH
 import Data.Time.Calendar
-import qualified Data.Text as T
+import Relude
 
 data Term
   = Lit Lit
   | Var Var
-  | Lam Var Blang
-  | App Blang Blang
+  | Lam Var Term
+  | App Term Term
   | Container (Container Term)
-  deriving (Functor)
-
+ 
 data Lit 
   = Num Double
-  | Bool Boolean
-  | Text T.Text
+  | Bool Bool
+  | Text LText
   | Account Account
   | Amount Amount   
   | Trans Transaction
-  | DateRange DateRange
   | Date Date
-
-type Transactions =  ???
-
-type Query = ???
-
-data Containers a 
+ 
+data Container a 
   = List [a]
-
-data DateRange = DateRange
-  { startDate :: Date
-  , endDate   :: Date
-  } deriving (Show)
-
-type Date = Day
-
+  deriving (Functor, Foldable, Traversable)
+ 
+-- data DateRange = DateRange
+--   { startDate :: Date
+--   , endDate   :: Date
+--   } deriving (Show)
+-- 
+ 
 data Transaction = Transaction
   { date        :: Date
   , fromAccount :: Account
   , toAccount   :: Account
-  , description :: T.Text
+  , description :: Text
   , amount      :: Amount
   } deriving (Show, Eq, Ord)
 
-type Var = T.Text
+type Date = Day
 
+type Var = LText
+ 
 type Amount = Decimal
-
-type Account = T.Text 
-
--- DatePeriod ~ Fix $ ListF Date
---
--- implicit 
---  
---   (Date -> Bool) -> (Transaction -> Bool) --> Query
---
--- explicit
---
---   [Date] -> (Transaction -> Bool) --> Query
---
--- (+) :: Date -> Period -> DatePeriod
--- d + p = every p d `before` (d + 2*p)
---
--- (-) :: DatePeriod -> DatePeriod -> DatePeriod
--- p1 - p2 = sort $ p1 ++ p2
---
--- every p d0 :: Period -> Date -> DatePeriod
--- every p = unfold (\d -> (d, d + p))
---
--- before :: DatePeriod -> Date -> DatePeriod
--- before dp d0 = 
-
--- Builtins / Algrebra
--- ---------------------
---
--- liftAccumPred     :: (Transactions -> Bool) -> Query
--- liftPred          :: (Transaction -> Bool) -> Query
--- modifyTrans       :: (Transaction -> Transaction) -> Query -> Query
--- modifyQueryName   :: (Text -> Text) -> Query -> Query
--- and, or           :: Query -> Query -> Query
--- not               :: Query -> Query
---
--- like :: Text -> Regex -> Bool
--- (<, <=, =, >=, >) :: Num -> Num -> Bool 
---
--- betweenDate :: DateRange -> Date -> Bool
---
--- totalAmount :: Transactions -> Amount
---
--- from :: Account -> Account -> Bool
--- to   :: Account -> Account -> Bool
--- (~>) :: Account -> Account -> Transaction -> Bool
---
--- transDesc   :: Transaction -> Text
--- transAmount :: Transaction -> Amount
--- transDate   :: Transaction -> Date
--- transTo     :: Transaction -> Account
--- transFrom   :: Transaction -> Account
---
---
--- What are the properties of this algebra?
---
--- Evaluators / Misc
--- ------------
--- runQuery          :: Query -> Transactions -> QueryResult
--- report            :: QueryResult -> IO ()
--- mkTrans           :: Desc -> Account -> Account -> Amount -> Transaction
--- simulateRegex     :: Regex -> String
--- simulateTrans     :: Date -> Query -> Maybe Transaction
--- loadCSV           :: FilePath -> Transactions
--- simulate          :: DateRange -> Query -> Transactions
  
--- Text      :: Text
--- Bool      :: Bool
--- Regex     :: Regex
--- Account   :: NonEmpty Text
--- Amount    :: Double
--- Account   :: Text 
--- Date      :: Day
--- DateRange :: [Day]
--- Query     :: State Transactions Transactions
+type Account = Text 
  
--- It might be useful to make Queries 1st class citizens...
+makeBaseFunctor ''Term
 
-makeFree ''BlangF
-
-main = undefined
+main = putStrLn "blang"
